05fabf34cff2bc2b367ff29e78ee92d494028e18
Nasser Mohammed <cuervo@Nassers-Laptop.local>
2026-05-25 22:09:25 -0500

    basic layout of i/o for keyboard recording

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+int processSound();
+int recordSound();
 
 int main(void) {
-    printf("Hello, World!\n");
+    printf("Enter command (r=record, s=stop, q=quit)");
+    char input;
+    while (1) {
+        scanf(" %c", &input);
+
+        if (input == 'r') {
+            printf("Recording... \n");
+        }
+        else if (input == 's') {
+            printf("Recording stopped \n");
+            processSound();
+            break;
+        }
+        else if (input == 'q') {
+            printf("Quitting program");
+            return 0;
+        }
+    }
+    return 0;
+}
+
+int processSound() {
+    return 0;
+}
+
+int recordSound() {
     return 0;
 }
